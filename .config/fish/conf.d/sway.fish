02cd1520670ba508f51a4f8aa571ac0910ceebab
István Popovics <ipopovics06@gmail.com>
2021-09-19 17:40:36 +0200

    Changed bg colors to darker ones

--- .config/fish/conf.d/sway.fish
+++
@@ -1,3 +0,0 @@
-if test -z $DISPLAY; and test (tty) = "/dev/tty1"
-    sway
-end
--- .config/sway/swayfader.py
+++ .config/sway/swayfader.py
@@ -1,231 +1,156 @@
-from i3ipc import Connection, Event
-from threading import Thread
-from time import sleep
+import asyncio
+import dataclasses
+from dataclasses import dataclass, field
+import os
+from pathlib import Path
+from typing import Dict, List, Optional
 
-# delay per frame
-FRAME_T = 0.01
-
-# transparency values
-CON_AC     = 1     # active window
-CON_INAC   = 0.7   # inactive window
-FLOAT_AC   = 1     # active floating window
-FLOAT_INAC = 0.95  # inactive floating window
-BOT_INAC   = 0.9   # bottom window
+from i3ipc.aio import Connection
+from i3ipc import Con, Event
+import toml
 
 
-# fade durations
-FADE_TIME      = 0.2
-ALT_FADE_TIME  = 0.1
-
-CON_OUT        = FADE_TIME      # window fading out
-CON_IN         = 0.15           # window fading in
-FLOAT_OUT      = ALT_FADE_TIME  # floating window fading out
-FLOAT_IN       = ALT_FADE_TIME  # floating window fading in
-BOT_OUT        = ALT_FADE_TIME  # bottom window fading out
-BOT_IN         = ALT_FADE_TIME  # bottom window fading in
-BOT_SWITCH_IN  = FADE_TIME      # window becoming bottom window
-BOT_SWITCH_OUT = FADE_TIME      # bottom window becoming window
-FLOAT_BOT_OUT  = FADE_TIME      # floating window fading out from bottom
-FLOAT_BOT_IN   = FADE_TIME      # floating window fading in from bottom
+@dataclass
+class Rule:
+    app_id: str
+    floating: Optional[bool]
+    focused: float
+    unfocused: float
 
 
-class Fader:
-    def __init__(self):
-        self.floating_windows = []
-        self.fader_running    = False
-        self.fade_queue       = []
-        self.fade_data        = {}
-        self.bottom_win       = None
-        self.old_win          = None
-        self.active_win       = None
-
-    def start(self):
-        ipc = Connection()
-        ipc.on(Event.WINDOW_FOCUS,    self.on_window_focus)
-        ipc.on(Event.WINDOW_NEW,      self.on_window_new)
-        ipc.on(Event.WINDOW_FLOATING, self.on_window_floating)
-
-        for win in ipc.get_tree():
-            if win.type == "floating_con":
-                self.floating_windows.append(win.id)
-                if win.focused:
-                    change_opacity(win, FLOAT_AC)
-                    self.active_win = win
-                else:
-                    change_opacity(win, FLOAT_INAC)
-            elif win.type == "con":
-                if win.focused:
-                    self.active_win = win
-                    change_opacity(win, CON_AC)
-                else:
-                    change_opacity(win, CON_INAC)
-
-        ipc.main()
-
-    def add_fade(self, win, start, target, duration):
-        if not duration:
-            if win.id in self.fade_queue:
-                self.fade_queue.remove(win.id)
-                del self.fade_data[win.id]
-            change_opacity(win, target)
-            return
-
-        if win.id in self.fade_queue:
-            f = self.fade_data[win.id]
-            change = (FRAME_T / duration) * (target - f["opacity"])
-            f["change"] = change
-            f["target"] = target
-            return
-
-        change_opacity(win, start)
-        change = (FRAME_T / duration) * (target - start)
-        fade_data = {"opacity": start, "change": change, "target": target, "win": win}
-        self.fade_queue.append(win.id)
-        self.fade_data[win.id] = fade_data
-
-    def start_fader(self):
-        if not self.fader_running:
-            self.fader_running = True
-            Thread(target=self.fader).start()
-
-    def fader(self):
-        while self.fade_queue:
-            for win_id in self.fade_queue.copy():
-                try:
-                    f = self.fade_data[win_id]
-                except KeyError:
-                    continue
-                f["opacity"] += f["change"]
-
-                finished = False
-                if f["change"] > 0:
-                    if f["opacity"] >= f["target"]:
-                        finished = True
-                elif f["opacity"] <= f["target"]:
-                    finished = True
-
-                if finished:
-                    change_opacity(f["win"], f["target"])
-                    try:
-                        self.fade_queue.remove(win_id)
-                        del self.fade_data[win_id]
-                    except (KeyError, ValueError):
-                        continue
-                else:
-                    change_opacity(f["win"], f["opacity"])
-
-            sleep(FRAME_T)
-        self.fader_running = False
-
-    def on_window_focus(self, ipc, e):
-        if self.active_win.id == e.containerid:
-            return
-
-        if self.active_win.type == "con":
-            if e.container.type == "con":
-                self.add_fade(
-                    e.container, CON_INAC,
-                    CON_AC, CON_IN)
-                self.add_fade(
-                    self.active_win, CON_AC,
-                    CON_INAC, CON_OUT)
-
-            else:
-                self.add_fade(
-                    e.container, FLOAT_INAC,
-                    FLOAT_AC, FLOAT_IN)
-                self.add_fade(
-                    self.active_win, CON_AC,
-                    BOT_INAC, BOT_OUT)
-                self.bottom_win = self.active_win
-
-        else:
-            if e.container.type == "con":
-                self.add_fade(
-                    self.active_win, FLOAT_AC,
-                    FLOAT_INAC, FLOAT_BOT_OUT)
-
-                if not self.bottom_win:
-                    self.add_fade(
-                        e.container, CON_INAC,
-                        CON_AC, CON_IN)
-
-                elif e.container.id != self.bottom_win.id:
-                    self.add_fade(
-                        self.bottom_win, BOT_INAC,
-                        CON_INAC, BOT_SWITCH_OUT)
-                    self.add_fade(
-                        e.container, CON_INAC,
-                        CON_AC, BOT_SWITCH_IN)
-                    self.bottom_win = e.container
-
-                else:
-                    self.add_fade(
-                        self.bottom_win, BOT_INAC,
-                        CON_AC, BOT_IN)
-
-            else:
-                self.add_fade(
-                    self.active_win, FLOAT_AC,
-                    FLOAT_INAC, FLOAT_OUT)
-                self.add_fade(
-                    e.container, FLOAT_INAC,
-                    FLOAT_AC, FLOAT_IN)
-
-        self.start_fader()
-        self.active_win = e.container
-
-    def on_window_new(self, ipc, e):
-        if self.active_win:
-            if self.active_win.type == "con":
-                change_opacity(self.active_win, CON_INAC)
-            else:
-                change_opacity(self.active_win, FLOAT_INAC)
-
-        if self.bottom_win:
-            change_opacity(self.bottom_win, CON_INAC)
-
-        elif self.active_win and self.active_win.type == "con":
-            self.bottom_win = self.active_win
-            change_opacity(self.bottom_win, CON_INAC)
-
-        change_opacity(e.container, CON_AC)
-        self.old_win = self.active_win
-        self.active_win = e.container
-
-    def on_window_floating(self, ipc, e):
-        c_id = e.container.id
-        if c_id not in self.floating_windows:
-            self.floating_windows.append(c_id)
-
-            if self.active_win.id != e.container.id:
-                change_opacity(e.container, FLOAT_INAC)
-
-            else:
-                if self.old_win and self.bottom_win:
-                    if self.old_win.type == "con":
-                        self.bottom_win = self.old_win
-                    change_opacity(self.bottom_win, BOT_INAC)
-                change_opacity(e.container, FLOAT_AC)
-
-        else:
-            self.floating_windows.remove(c_id)
-            if self.active_win.id != e.container.id:
-                change_opacity(e.container, CON_INAC)
-
-            else:
-                if self.old_win and self.old_win.type == "con":
-                    change_opacity(self.old_win, CON_INAC)
-                change_opacity(self.active_win, CON_AC)
-
-        self.active_win = e.container
+@dataclass
+class Config:
+    focused: float = 1
+    unfocused: float = 0.98
+    rate: float = 0.016
+    steps: float = 14
+    rules: List[Rule] = field(default_factory=list)
 
 
-def change_opacity(win, trans):
-    win.command("opacity " + str(trans))
+cfg = Config()
+cfg_basedir = Path(os.getenv("XDG_CONFIG_HOME") or (Path.home() / ".config"))
+cfg_path = cfg_basedir / "swayfade.toml"
 
 
-if __name__ == "__main__":
-    f = Fader()
-    f.start()
+if cfg_path.exists():
+    with open(cfg_path) as f:
+        cfg_dict = toml.load(f)
+        default_rule = Rule(app_id="", floating=None, focused=1, unfocused=1)
+        cfg_dict["rules"] = [
+            dataclasses.replace(default_rule, **r) for r in cfg_dict["rules"]
+        ]
+        cfg = dataclasses.replace(cfg, **cfg_dict)
+
+cfg.steps = 1 / cfg.steps
+
+
+def lerp(start: float, end: float, step: float):
+    return start + step * (end - start)
+
+
+async def set_opacity(c: Con, f: float):
+    await c.command(f"opacity {f}")
+
+
+@dataclass
+class Container:
+    opacity: float
+    task: Optional[asyncio.Task]
+    focused: float
+    unfocused: float
+
+
+# globals
+focused: Con
+container: Dict[int, Container] = dict()
+
+
+def get_opacity_rule(win: Con):
+    floating = win.type == "floating_con"
+    rules = [
+        r
+        for r in cfg.rules
+        if (not r.app_id or win.app_id == r.app_id)
+        and (
+            (r.floating is None) or (r.floating is not None and floating == r.floating)
+        )
+    ]
+    return (
+        (rules[0].focused, rules[0].unfocused)
+        if rules
+        else (cfg.focused, cfg.unfocused)
+    )
+
+
+async def init_container(con):
+    if con.type in ["con", "floating_con"]:
+        f, u = get_opacity_rule(con)
+        opacity = f if con == focused else u
+        container[con.id] = Container(
+            opacity=opacity, task=None, focused=f, unfocused=u
+        )
+        await set_opacity(con, opacity)
+
+
+async def fade(con: Con, con_data: Container, focused: bool):
+    start = con_data.opacity
+    end = con_data.focused if focused else con_data.unfocused
+    o = start
+    step = 0.0
+    while abs(o - end) > cfg.steps:
+        o = lerp(start, end, step)
+        step += cfg.steps
+        await set_opacity(con, o)
+        con_data.opacity = o
+        await asyncio.sleep(cfg.rate)
+    await set_opacity(con, end)
+    con_data.opacity = end
+
+
+def reset_fade(con: Con, focused: bool):
+    # create container data if it doesn't exist
+    global container
+    con_data = container.get(con.id)
+    if con_data is None:
+        focused, unfocused = get_opacity_rule(con)
+        con_data = Container(
+            opacity=unfocused, focused=focused, unfocused=unfocused, task=None
+        )
+        container[con.id] = con_data
+
+    # cancel an existing fade task if one exists
+    task = con_data.task
+    if task:
+        task.cancel()
+    con_data.task = asyncio.create_task(fade(con, con_data, focused))
+
+
+def on_window_focus(c: Connection, e: Event):
+    global focused
+    # skips firing when focusing from parent
+    if focused is None or focused.id == e.container.id:
+        return
+
+    reset_fade(focused, False)
+    reset_fade(e.container, True)
+    focused = e.container
+
+
+def on_window_floating(c: Connection, e: Event):
+    asyncio.create_task(init_container(e.container))
+
+
+async def main():
+    connection = await Connection(auto_reconnect=True).connect()
+    tree = await connection.get_tree()
+    global focused
+    focused = [con for con in tree if con.focused][0]
+    await asyncio.gather(*[init_container(con) for con in tree])
+    connection.on(Event.WINDOW_FOCUS, on_window_focus)
+    connection.on(Event.WINDOW_FLOATING, on_window_floating)
+    await connection.main()
+
+
+asyncio.get_event_loop().run_until_complete(main())
+
 
