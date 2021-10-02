d5dc64cc197500419e15e289debcbf6c6a210976
István Popovics <ipopovics06@gmail.com>
2021-10-02 14:00:20 +0200

    Version 2.1

--- .config/sway/rofi-power.sh
+++ .config/sway/rofi-power.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-entries=" Suspend\n Logout\n⏾ Suspend\n Reboot\n Shutdown"
+entries=" Hibernate\n Logout\n⏾ Suspend\n Reboot\n Shutdown"
 selected=$(echo -e $entries|rofi -dmenu | awk '{print tolower($2)}')
 
 case $selected in
-  suspend)
+  hibernate)
     exec systemctl hibernate;;
   logout)
     exec swaymsg exit;;
